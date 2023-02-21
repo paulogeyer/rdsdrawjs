5e1a3d8dcff06cb6bd3851abf58be470940c09e3
Paulo Geyer <paulogeyer@gmail.com>
2023-02-21 00:21:03 -0300

    freehand partial implementation

--- src/freehand.js
+++ src/freehand.js
@@ -4,11 +4,42 @@ class Freehand extends Tool {
     this.icon = loadImage('icons/freehand.png');
     this.name = 'freehand';
     this.desc = 'painting tool: FREHAND DRAWING (select with right mousebutton for settings)';
-    var startMouseX = -1;
-    var startMouseY = -1;
-    var drawing = false;
+
+    this.previousMouseX = -1;
+    this.previousMouseY = -1;
   }
 
   draw() {
+    //if the mouse is pressed
+    if(mouseIsPressed){
+      //check if they previousX and Y are -1. set them to the current
+      //mouse X and Y if they are.
+      if (this.previousMouseX == -1){
+	this.previousMouseX = mouseX;
+	this.previousMouseY = mouseY;
+      }
+      //if we already have values for previousX and Y we can draw a line from 
+      //there to the current mouse location
+      else{
+	canvas.canvas
+	  .stroke(toolbox.palette.colors[toolbox.palette.cur_fg][1]);
+	canvas.canvas
+	  .strokeWeight(30);
+	canvas.canvas
+	  .line(this.previousMouseX-canvas.x,
+		this.previousMouseY,
+		mouseX-canvas.x,
+		mouseY);
+	this.previousMouseX = mouseX;
+	this.previousMouseY = mouseY;
+      }
+    }
+    //if the user has released the mouse we want to set the previousMouse values 
+    //back to -1.
+    //try and comment out these lines and see what happens!
+    else{
+      this.previousMouseX = -1;
+      this.previousMouseY = -1;
+    }
   }
 }
