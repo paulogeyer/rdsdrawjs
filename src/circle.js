c555cd30fa83e9391645549dc264b1d2e6a405e7
Paulo Geyer <paulogeyer@gmail.com>
2023-02-20 21:38:37 -0300

    circle

--- src/circle.js
+++ src/circle.js
@@ -4,11 +4,33 @@ class Circle extends Tool {
     this.icon = loadImage('icons/circle.png');
     this.name = 'circle';
     this.desc = 'painting tool: CIRCLE';
-    var startMouseX = -1;
-    var startMouseY = -1;
-    var drawing = false;
+    this.startMouseX = -1;
+    this.startMouseY = -1;
+    this.drawing = false;
   }
 
   draw() {
+    if(mouseIsPressed) {
+      if(this.startMouseX == -1){
+	this.startMouseX = mouseX;
+	this.startMouseY = mouseY;
+	this.drawing = true;
+	canvas.canvas.loadPixels();
+      } else {
+	// draw the line when the user release the mouse button
+	canvas.canvas.updatePixels();
+	canvas.canvas.noStroke();
+	canvas.canvas.fill(
+	  toolbox.palette.colors[toolbox.palette.cur_fg][1]);
+	canvas.canvas.ellipse(this.startMouseX-canvas.x,
+			      this.startMouseY,
+			      2*(mouseX-this.startMouseX),
+			      2*(mouseY-this.startMouseY));
+      }
+    } else if(this.drawing) {
+      this.drawing = false;
+      this.startMouseX = -1;
+      this.startMouseY = -1;
+    }
   }
 }
