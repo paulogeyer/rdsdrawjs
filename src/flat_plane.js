05df1d666ba9be5a9ab7ab0dc708c8727d83fdac
Paulo Geyer <paulogeyer@gmail.com>
2023-02-23 13:39:23 -0300

    partial behavior for flat plane implemented

--- src/flat_plane.js
+++ src/flat_plane.js
@@ -4,33 +4,75 @@ class FlatPlane extends Tool {
     this.icon = loadImage('icons/flat_plane.png');
     this.name = 'flat plane';
     this.desc = 'painting tool: FLAT PLANE';
-    this.startMouseX = -1;
-    this.startMouseY = -1;
+    this.pt1x = -1;
+    this.pt1y = -1;
+    this.pt2x = -1;
+    this.pt2y = -1;
     this.drawing = false;
+    this.step = 0;
   }
 
   draw() {
-    if(mouseIsPressed) {
-      if(this.startMouseX == -1){
-	this.startMouseX = mouseX;
-	this.startMouseY = mouseY;
+    if(this.step == 0) {
+      if(mouseIsPressed) {
+	this.pt1x = mouseX;
+	this.pt1y = mouseY;
 	this.drawing = true;
+	this.step = 1;
 	canvas.canvas.loadPixels();
-      } else {
+      }
+    } else if (this.step == 1) {
+      if(mouseIsPressed) {
 	// draw the line when the user release the mouse button
 	canvas.canvas.updatePixels();
-	canvas.canvas.noStroke();
-	canvas.canvas.fill(
+	// canvas.canvas.noStroke();
+	canvas.canvas.strokeWeight(2);
+	canvas.canvas.stroke(
 	  toolbox.palette.colors[toolbox.palette.cur_fg][1]);
-	canvas.canvas.rect(this.startMouseX-canvas.x,
-			   this.startMouseY,
-			   mouseX-this.startMouseX,
-			   mouseY-this.startMouseY);
+	canvas.canvas.line(this.pt1x-canvas.x,
+			   this.pt1y,
+			   mouseX-canvas.x,
+			   mouseY);
+	this.pt2x = mouseX;
+	this.pt2y = mouseY;
+
+	// canvas.canvas.rect(this.startMouseX-canvas.x,
+	// 		   this.startMouseY,
+	// 		   mouseX-this.startMouseX,
+	// 		   mouseY-this.startMouseY);
+      } else {
+	this.step = 2;
       }
-    } else if(this.drawing) {
-      this.drawing = false;
-      this.startMouseX = -1;
-      this.startMouseY = -1;
+    } else if (this.step == 2) {
+      var pt3x = mouseX+this.pt2x-canvas.x-this.pt1x-canvas.x;
+      var pt3y = mouseY+this.pt2y-this.pt1y-canvas.y;
+      canvas.canvas.updatePixels();
+      canvas.canvas.beginShape();
+      canvas.canvas.noFill();
+      canvas.canvas.vertex(this.pt1x-canvas.x, this.pt1y-canvas.y);
+      canvas.canvas.vertex(this.pt2x-canvas.x, this.pt2y);
+      canvas.canvas.vertex(mouseX-canvas.x, mouseY-canvas.y);
+      canvas.canvas.vertex(pt3x, pt3y);
+      canvas.canvas.endShape();
     }
+
+    // } else if (this.drawing && this.step == 1) {
+    //   var pt3x = this.pt2x-canvas.x-this.pt1x-canvas.x;
+    //   var pt3y = this.pt2y-this.pt1y-canvas.y;
+    //   canvas.canvas.updatePixels();
+    //   canvas.canvas.beginShape();
+    //   canvas.canvas.vertex(this.pt1x-canvas.x, this.pt1y-canvas.y);
+    //   canvas.canvas.vertex(this.pt2x-canvas.x, this.pt2y);
+    //   canvas.canvas.vertex(mouseX-canvas.x, mouseY-canvas.y);
+    //   canvas.canvas.vertex(pt3x, pt3y);
+    //   canvas.canvas.endShape();
+    // } else if(this.drawing) {
+    //   this.drawing = false;
+    //   this.step = 0;
+    //   this.pt1x = -1;
+    //   this.pt1y = -1;
+    //   this.pt2x = -1;
+    //   this.pt2y = -1;
+    // }
   }
 }
